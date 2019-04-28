991e615d2ac59f884b73b35f1e887d14bf98e03b
maciejpajkowski <32037952+maciejpajkowski@users.noreply.github.com>
2019-04-28 16:22:18 +0200

    Improved file organization, introduced enums for connection strings and other changes

--- TrackerLibrary/DataAccess/TextConnector.cs
+++ TrackerLibrary/DataAccess/TextConnector.cs
@@ -5,16 +5,19 @@ using System.Text;
 using System.Threading.Tasks;
 using TrackerLibrary.Models;
 
-namespace TrackerLibrary
+namespace TrackerLibrary.DataAccess
 {
     public class TextConnector : IDataConnection
     {
         // TODO - Wire up the CreatePrize method for text files.
         public PrizeModel CreatePrize(PrizeModel model)
         {
-            model.Id = 1;
-
-            return model;
+            // Load the text file
+            // Convert the text file to List<PrizeModel>
+            // Find the ID
+            // Add the new record with the new ID (max + 1)
+            // Convert the prizes to List<string>
+            // Save the List<string> to the text file
         }
     }
 }
--- TrackerLibrary/Enums.cs
+++ TrackerLibrary/Enums.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using TrackerLibrary.Models;
 
 namespace TrackerLibrary
 {
-    public interface IDataConnection
+    public enum DatabaseType
     {
-        PrizeModel CreatePrize(PrizeModel model);
+        Sql,
+        TextFile
     }
 }
